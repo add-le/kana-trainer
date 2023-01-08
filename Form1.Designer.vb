a9131fb7a61fdc8ff27095fb60143fe442a3c6bc
add-le <add.lelann@pm.me>
2023-01-08 22:39:26 +0100

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.KanaOutput = New System.Windows.Forms.Label()
+        Me.AnswerInput = New System.Windows.Forms.TextBox()
+        Me.TimerNextKana = New System.Windows.Forms.Timer(Me.components)
+        Me.KanaSelector = New System.Windows.Forms.CheckedListBox()
+        Me.Success1 = New System.Windows.Forms.PictureBox()
+        Me.Success2 = New System.Windows.Forms.PictureBox()
+        Me.Success3 = New System.Windows.Forms.PictureBox()
+        Me.Success4 = New System.Windows.Forms.PictureBox()
+        Me.Success5 = New System.Windows.Forms.PictureBox()
+        Me.Success10 = New System.Windows.Forms.PictureBox()
+        Me.Success9 = New System.Windows.Forms.PictureBox()
+        Me.Success8 = New System.Windows.Forms.PictureBox()
+        Me.Success7 = New System.Windows.Forms.PictureBox()
+        Me.Success6 = New System.Windows.Forms.PictureBox()
+        CType(Me.Success1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Success6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'KanaOutput
+        '
+        Me.KanaOutput.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.KanaOutput.Font = New System.Drawing.Font("Yu Mincho", 72.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.KanaOutput.Location = New System.Drawing.Point(12, 45)
+        Me.KanaOutput.Name = "KanaOutput"
+        Me.KanaOutput.Size = New System.Drawing.Size(776, 124)
+        Me.KanaOutput.TabIndex = 0
+        Me.KanaOutput.Text = "あ"
+        Me.KanaOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'AnswerInput
+        '
+        Me.AnswerInput.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.AnswerInput.BackColor = System.Drawing.Color.White
+        Me.AnswerInput.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.AnswerInput.Font = New System.Drawing.Font("Microsoft YaHei", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.AnswerInput.Location = New System.Drawing.Point(12, 172)
+        Me.AnswerInput.Name = "AnswerInput"
+        Me.AnswerInput.Size = New System.Drawing.Size(776, 47)
+        Me.AnswerInput.TabIndex = 1
+        Me.AnswerInput.Text = "a"
+        Me.AnswerInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TimerNextKana
+        '
+        Me.TimerNextKana.Interval = 500
+        '
+        'KanaSelector
+        '
+        Me.KanaSelector.BackColor = System.Drawing.SystemColors.Window
+        Me.KanaSelector.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.KanaSelector.CheckOnClick = True
+        Me.KanaSelector.FormattingEnabled = True
+        Me.KanaSelector.Items.AddRange(New Object() {"Hiragana", "Hiragana Dakuten", "Hiragana Combo", "Katakana", "Katakana Dakuten", "Katakana Combo", "Katakana Extended"})
+        Me.KanaSelector.Location = New System.Drawing.Point(16, 308)
+        Me.KanaSelector.Name = "KanaSelector"
+        Me.KanaSelector.Size = New System.Drawing.Size(138, 126)
+        Me.KanaSelector.TabIndex = 114
+        '
+        'Success1
+        '
+        Me.Success1.BackColor = System.Drawing.Color.Gray
+        Me.Success1.Location = New System.Drawing.Point(322, 419)
+        Me.Success1.Name = "Success1"
+        Me.Success1.Size = New System.Drawing.Size(10, 10)
+        Me.Success1.TabIndex = 115
+        Me.Success1.TabStop = False
+        '
+        'Success2
+        '
+        Me.Success2.BackColor = System.Drawing.Color.Gray
+        Me.Success2.Location = New System.Drawing.Point(338, 419)
+        Me.Success2.Name = "Success2"
+        Me.Success2.Size = New System.Drawing.Size(10, 10)
+        Me.Success2.TabIndex = 116
+        Me.Success2.TabStop = False
+        '
+        'Success3
+        '
+        Me.Success3.BackColor = System.Drawing.Color.Gray
+        Me.Success3.Location = New System.Drawing.Point(354, 419)
+        Me.Success3.Name = "Success3"
+        Me.Success3.Size = New System.Drawing.Size(10, 10)
+        Me.Success3.TabIndex = 117
+        Me.Success3.TabStop = False
+        '
+        'Success4
+        '
+        Me.Success4.BackColor = System.Drawing.Color.Gray
+        Me.Success4.Location = New System.Drawing.Point(370, 419)
+        Me.Success4.Name = "Success4"
+        Me.Success4.Size = New System.Drawing.Size(10, 10)
+        Me.Success4.TabIndex = 118
+        Me.Success4.TabStop = False
+        '
+        'Success5
+        '
+        Me.Success5.BackColor = System.Drawing.Color.Gray
+        Me.Success5.Location = New System.Drawing.Point(386, 419)
+        Me.Success5.Name = "Success5"
+        Me.Success5.Size = New System.Drawing.Size(10, 10)
+        Me.Success5.TabIndex = 119
+        Me.Success5.TabStop = False
+        '
+        'Success10
+        '
+        Me.Success10.BackColor = System.Drawing.Color.Gray
+        Me.Success10.Location = New System.Drawing.Point(466, 419)
+        Me.Success10.Name = "Success10"
+        Me.Success10.Size = New System.Drawing.Size(10, 10)
+        Me.Success10.TabIndex = 124
+        Me.Success10.TabStop = False
+        '
+        'Success9
+        '
+        Me.Success9.BackColor = System.Drawing.Color.Gray
+        Me.Success9.Location = New System.Drawing.Point(450, 419)
+        Me.Success9.Name = "Success9"
+        Me.Success9.Size = New System.Drawing.Size(10, 10)
+        Me.Success9.TabIndex = 123
+        Me.Success9.TabStop = False
+        '
+        'Success8
+        '
+        Me.Success8.BackColor = System.Drawing.Color.Gray
+        Me.Success8.Location = New System.Drawing.Point(434, 419)
+        Me.Success8.Name = "Success8"
+        Me.Success8.Size = New System.Drawing.Size(10, 10)
+        Me.Success8.TabIndex = 122
+        Me.Success8.TabStop = False
+        '
+        'Success7
+        '
+        Me.Success7.BackColor = System.Drawing.Color.Gray
+        Me.Success7.Location = New System.Drawing.Point(418, 419)
+        Me.Success7.Name = "Success7"
+        Me.Success7.Size = New System.Drawing.Size(10, 10)
+        Me.Success7.TabIndex = 121
+        Me.Success7.TabStop = False
+        '
+        'Success6
+        '
+        Me.Success6.BackColor = System.Drawing.Color.Gray
+        Me.Success6.Location = New System.Drawing.Point(402, 419)
+        Me.Success6.Name = "Success6"
+        Me.Success6.Size = New System.Drawing.Size(10, 10)
+        Me.Success6.TabIndex = 120
+        Me.Success6.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Success10)
+        Me.Controls.Add(Me.Success9)
+        Me.Controls.Add(Me.Success8)
+        Me.Controls.Add(Me.Success7)
+        Me.Controls.Add(Me.Success6)
+        Me.Controls.Add(Me.Success5)
+        Me.Controls.Add(Me.Success4)
+        Me.Controls.Add(Me.Success3)
+        Me.Controls.Add(Me.Success2)
+        Me.Controls.Add(Me.Success1)
+        Me.Controls.Add(Me.KanaSelector)
+        Me.Controls.Add(Me.AnswerInput)
+        Me.Controls.Add(Me.KanaOutput)
+        Me.MaximumSize = New System.Drawing.Size(816, 489)
+        Me.MinimumSize = New System.Drawing.Size(816, 489)
+        Me.Name = "Form1"
+        Me.Text = "KanaTrainer"
+        CType(Me.Success1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Success6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents KanaOutput As Label
+    Friend WithEvents AnswerInput As TextBox
+    Friend WithEvents TimerNextKana As Timer
+    Friend WithEvents KanaSelector As CheckedListBox
+    Friend WithEvents Success1 As PictureBox
+    Friend WithEvents Success2 As PictureBox
+    Friend WithEvents Success3 As PictureBox
+    Friend WithEvents Success4 As PictureBox
+    Friend WithEvents Success5 As PictureBox
+    Friend WithEvents Success10 As PictureBox
+    Friend WithEvents Success9 As PictureBox
+    Friend WithEvents Success8 As PictureBox
+    Friend WithEvents Success7 As PictureBox
+    Friend WithEvents Success6 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,455 @@
+﻿Public Class Form1
+
+    ' Number of kana
+    Private Const KANA_SIZE As Integer = 46
+    Private Const KANA_DAKUTEN_SIZE As Integer = 25
+    Private Const KANA_COMBO_SIZE As Integer = 33
+
+    ' Choice of kana
+    Private Const HIRAGANA As Integer = 0
+    Private Const HIRAGANA_DAKUTEN As Integer = 1
+    Private Const HIRAGANA_COMBO As Integer = 2
+    Private Const KATAKANA As Integer = 3
+    Private Const KATAKANA_DAKUTEN As Integer = 4
+    Private Const KATAKANA_COMBO As Integer = 5
+    Private Const KATAKANA_EXTENDED As Integer = 6
+
+    ' Current selection
+    Dim kanaType As Integer = HIRAGANA
+
+    ' Current kana used
+    Private hiraganaEnabled As Boolean = False
+    Private hiraganaDakutenEnabled As Boolean = False
+    Private hiraganaComboEnabled As Boolean = False
+    Private katakanaEnabled As Boolean = False
+    Private katakanaDakutenEnabled As Boolean = False
+    Private katakanaComboEnabled As Boolean = False
+    Private katakanaExtendedEnabled As Boolean = False
+
+    ' List of all hiragana
+    ReadOnly hiraganaTable As New List(Of (Kana As String, Transcription As String, Success As Integer)) From {
+        ("あ", "a", 0),
+        ("い", "i", 0),
+        ("う", "u", 0),
+        ("え", "e", 0),
+        ("お", "o", 0),
+        ("か", "ka", 0),
+        ("き", "ki", 0),
+        ("く", "ku", 0),
+        ("け", "ke", 0),
+        ("こ", "ko", 0),
+        ("さ", "sa", 0),
+        ("し", "shi", 0),
+        ("す", "su", 0),
+        ("せ", "se", 0),
+        ("そ", "so", 0),
+        ("た", "ta", 0),
+        ("ち", "chi", 0),
+        ("つ", "tsu", 0),
+        ("て", "te", 0),
+        ("と", "to", 0),
+        ("な", "na", 0),
+        ("に", "ni", 0),
+        ("ぬ", "nu", 0),
+        ("ね", "ne", 0),
+        ("の", "no", 0),
+        ("は", "ha", 0),
+        ("ひ", "hi", 0),
+        ("ふ", "fu", 0),
+        ("へ", "he", 0),
+        ("ほ", "ho", 0),
+        ("ま", "ma", 0),
+        ("み", "mi", 0),
+        ("む", "mu", 0),
+        ("め", "me", 0),
+        ("も", "mo", 0),
+        ("や", "ya", 0),
+        ("ゆ", "yu", 0),
+        ("よ", "yo", 0),
+        ("ら", "ra", 0),
+        ("り", "ri", 0),
+        ("る", "ru", 0),
+        ("れ", "re", 0),
+        ("ろ", "ro", 0),
+        ("わ", "wa", 0),
+        ("を", "wo", 0),
+        ("ん", "n", 0)
+    }
+    ReadOnly hiraganaDakutenTable As New List(Of (Kana As String, Transcription As String, Success As Integer)) From {
+        ("が", "ga", 0),
+        ("ぎ", "gi", 0),
+        ("ぐ", "gu", 0),
+        ("げ", "ge", 0),
+        ("ご", "go", 0),
+        ("ざ", "za", 0),
+        ("じ", "ji", 0),
+        ("ず", "zu", 0),
+        ("ぜ", "ze", 0),
+        ("ぞ", "zo", 0),
+        ("だ", "da", 0),
+        ("ぢ", "ji", 0),
+        ("づ", "zu", 0),
+        ("で", "de", 0),
+        ("ど", "do", 0),
+        ("ば", "ba", 0),
+        ("び", "bi", 0),
+        ("ぶ", "bu", 0),
+        ("べ", "be", 0),
+        ("ぼ", "bo", 0),
+        ("ぱ", "pa", 0),
+        ("ぴ", "pi", 0),
+        ("ぷ", "pu", 0),
+        ("ぺ", "pe", 0),
+        ("ぽ", "po", 0)
+    }
+    ReadOnly hiraganaComboTable As New List(Of (Kana As String, Transcription As String, Success As Integer)) From {
+        ("きゃ", "kya", 0),
+        ("きゅ", "kyu", 0),
+        ("きょ", "kyo", 0),
+        ("ぎゃ", "gya", 0),
+        ("ぎゅ", "gyu", 0),
+        ("ぎょ", "gyo", 0),
+        ("しゃ", "sha", 0),
+        ("しゅ", "shu", 0),
+        ("しょ", "sho", 0),
+        ("じゃ", "ja", 0),
+        ("じゅ", "ju", 0),
+        ("じょ", "jo", 0),
+        ("ちゃ", "cha", 0),
+        ("ちゅ", "chu", 0),
+        ("ちょ", "cho", 0),
+        ("にゃ", "nya", 0),
+        ("にゅ", "nyu", 0),
+        ("にょ", "nyo", 0),
+        ("ひゃ", "hya", 0),
+        ("ひゅ", "hyu", 0),
+        ("ひょ", "hyo", 0),
+        ("びゃ", "bya", 0),
+        ("びゅ", "byu", 0),
+        ("びょ", "byo", 0),
+        ("ぴゃ", "pya", 0),
+        ("ぴゅ", "pyu", 0),
+        ("ぴょ", "pyo", 0),
+        ("みゃ", "mya", 0),
+        ("みゅ", "myu", 0),
+        ("みょ", "myo", 0),
+        ("りゃ", "rya", 0),
+        ("りゅ", "ryu", 0),
+        ("りょ", "ryo", 0)
+    }
+
+    ' List of all katakana
+    ReadOnly katakanaTable As New List(Of (Kana As String, Transcription As String, Success As Integer)) From {
+        ("ア", "a", 0),
+        ("イ", "i", 0),
+        ("ウ", "u", 0),
+        ("エ", "e", 0),
+        ("オ", "o", 0),
+        ("カ", "ka", 0),
+        ("キ", "ki", 0),
+        ("ク", "ku", 0),
+        ("ケ", "ke", 0),
+        ("コ", "ko", 0),
+        ("サ", "sa", 0),
+        ("シ", "shi", 0),
+        ("ス", "su", 0),
+        ("セ", "se", 0),
+        ("ソ", "so", 0),
+        ("タ", "ta", 0),
+        ("チ", "chi", 0),
+        ("ツ", "tsu", 0),
+        ("テ", "te", 0),
+        ("ト", "to", 0),
+        ("ナ", "na", 0),
+        ("ニ", "ni", 0),
+        ("ヌ", "nu", 0),
+        ("ネ", "ne", 0),
+        ("ノ", "no", 0),
+        ("ハ", "ha", 0),
+        ("ヒ", "hi", 0),
+        ("フ", "fu", 0),
+        ("ヘ", "he", 0),
+        ("ホ", "ho", 0),
+        ("マ", "ma", 0),
+        ("ミ", "mi", 0),
+        ("ム", "mu", 0),
+        ("メ", "me", 0),
+        ("モ", "mo", 0),
+        ("ヤ", "ya", 0),
+        ("ユ", "yu", 0),
+        ("ヨ", "yo", 0),
+        ("ラ", "ra", 0),
+        ("リ", "ri", 0),
+        ("ル", "ru", 0),
+        ("レ", "re", 0),
+        ("ロ", "ro", 0),
+        ("ワ", "wa", 0),
+        ("ヲ", "wo", 0),
+        ("ン", "n", 0)
+    }
+    ReadOnly katakanaDakutenTable As New List(Of (Kana As String, Transcription As String, Success As Integer)) From {
+        ("ガ", "ga", 0),
+        ("ギ", "gi", 0),
+        ("グ", "gu", 0),
+        ("ゲ", "ge", 0),
+        ("ゴ", "go", 0),
+        ("ザ", "za", 0),
+        ("ジ", "ji", 0),
+        ("ズ", "zu", 0),
+        ("ゼ", "zo", 0),
+        ("ゾ", "zo", 0),
+        ("ダ", "da", 0),
+        ("ヂ", "ji", 0),
+        ("ヅ", "zu", 0),
+        ("デ", "de", 0),
+        ("ド", "do", 0),
+        ("バ", "ba", 0),
+        ("ビ", "bi", 0),
+        ("ブ", "bu", 0),
+        ("ベ", "be", 0),
+        ("ボ", "bo", 0),
+        ("パ", "pa", 0),
+        ("ピ", "pi", 0),
+        ("プ", "pu", 0),
+        ("ペ", "pe", 0),
+        ("ポ", "po", 0)
+    }
+    ReadOnly katakanaComboTable As New List(Of (Kana As String, Transcription As String, Success As Integer)) From {
+        ("キャ", "kya", 0),
+        ("キュ", "kyu", 0),
+        ("キョ", "kyo", 0),
+        ("ギャ", "gya", 0),
+        ("ギュ", "gyu", 0),
+        ("ギョ", "gyo", 0),
+        ("シャ", "sha", 0),
+        ("シュ", "shu", 0),
+        ("ショ", "sho", 0),
+        ("ジャ", "ja", 0),
+        ("ジュ", "ju", 0),
+        ("ジョ", "jo", 0),
+        ("チャ", "cha", 0),
+        ("チュ", "chu", 0),
+        ("チョ", "cho", 0),
+        ("ニャ", "nya", 0),
+        ("ニュ", "nyu", 0),
+        ("ニョ", "nyo", 0),
+        ("ヒャ", "hya", 0),
+        ("ヒュ", "hyu", 0),
+        ("ヒョ", "hyo", 0),
+        ("ビャ", "bya", 0),
+        ("ビュ", "byu", 0),
+        ("ビョ", "byo", 0),
+        ("ピャ", "pya", 0),
+        ("ピュ", "pyu", 0),
+        ("ピョ", "pyo", 0),
+        ("ミャ", "mya", 0),
+        ("ミュ", "myu", 0),
+        ("ミョ", "myo", 0),
+        ("リャ", "rya", 0),
+        ("リュ", "ryu", 0),
+        ("リョ", "ryo", 0)
+    }
+
+    ' Random choice of kana
+    Dim rand As Integer
+
+    Private Function GetKanaTable() As List(Of (Kana As String, Transcription As String, Success As Integer))
+
+        Dim table As New List(Of (Kana As String, Transcription As String, Success As Integer))()
+
+        If hiraganaEnabled Then
+            table.AddRange(hiraganaTable)
+        End If
+
+        If hiraganaDakutenEnabled Then
+            table.AddRange(hiraganaDakutenTable)
+        End If
+
+        If hiraganaComboEnabled Then
+            table.AddRange(hiraganaComboTable)
+        End If
+
+        If katakanaEnabled Then
+            table.AddRange(katakanaTable)
+        End If
+
+        If katakanaDakutenEnabled Then
+            table.AddRange(katakanaDakutenTable)
+        End If
+
+        If katakanaComboEnabled Then
+            table.AddRange(katakanaComboTable)
+        End If
+
+        If katakanaExtendedEnabled Then
+
+        End If
+
+        Return table
+
+    End Function
+
+    Private Sub RandomKana()
+
+        Dim table = GetKanaTable()
+        rand = Int(Rnd() * table.Count)
+        KanaOutput.Text = table(rand).Kana
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' Set the default selector on hiragana
+        KanaSelector.SetItemChecked(0, True)
+        hiraganaEnabled = True
+
+        ' Initialize the random-number generator
+        Randomize()
+
+        ' Generate the first kana
+        RandomKana()
+
+    End Sub
+
+    Private Sub NextKana()
+
+        ' Reset the timer
+        TimerNextKana.Enabled = False
+
+        ' Reset to default color
+        AnswerInput.ForeColor = Color.Black
+        KanaOutput.ForeColor = Color.Black
+
+        ' Empty the input field
+        AnswerInput.Text = ""
+
+        ' Generate the next kana
+        RandomKana()
+
+    End Sub
+
+    Private Sub DisplayCorrection()
+        AnswerInput.Text = GetKanaTable(rand).Transcription
+    End Sub
+
+    Private Sub DisplaySuccessRate()
+        Dim table = GetKanaTable()
+        Dim successRate As Integer = table(rand).Success
+
+        For index = 1 To successRate
+            Controls.Find("Success" + index.ToString(), True)(0).BackColor = Color.Cyan
+        Next
+
+    End Sub
+
+    Private Sub AnswerInput_KeyDown(sender As Object, e As KeyEventArgs) Handles AnswerInput.KeyDown
+
+        ' Cannot valid if answer is empty
+        If AnswerInput.Text = "" Then
+            Exit Sub
+        End If
+
+        ' When Enter is press on the input field
+        If e.KeyCode = Keys.Enter Then
+            ' If the answer is correct
+            If AnswerInput.Text = GetKanaTable(rand).Transcription Then
+                AnswerInput.ForeColor = Color.Cyan
+                KanaOutput.ForeColor = Color.Cyan
+
+                ' Increase success
+                Dim successRate = GetKanaTable(rand).Success
+                GetKanaTable(rand) = (GetKanaTable(rand).Kana, GetKanaTable(rand).Transcription, successRate + 1)
+
+            Else
+                AnswerInput.ForeColor = Color.Salmon
+                KanaOutput.ForeColor = Color.Salmon
+
+                ' Decrease success
+                Dim successRate = GetKanaTable(rand).Success
+                GetKanaTable(rand) = (GetKanaTable(rand).Kana, GetKanaTable(rand).Transcription, successRate - 1)
+
+                ' Show the correction 
+                DisplayCorrection()
+
+            End If
+
+            ' Launch the delay of 500ms
+            TimerNextKana.Enabled = True
+
+        End If
+
+    End Sub
+
+    Private Sub TimerNextKana_Tick(sender As Object, e As EventArgs) Handles TimerNextKana.Tick
+
+        ' Generate the next kana
+        NextKana()
+
+    End Sub
+
+    Private Sub KanaSelector_ItemCheck(sender As Object, e As ItemCheckEventArgs) Handles KanaSelector.ItemCheck
+
+        ' Cannot disable the last item
+        If e.NewValue = CheckState.Unchecked Then
+            If KanaSelector.CheckedItems.Count = 1 Then
+                e.NewValue = e.CurrentValue
+            End If
+        End If
+
+    End Sub
+
+    Private Sub KanaSelector_SelectedIndexChanged(sender As Object, e As EventArgs) Handles KanaSelector.SelectedIndexChanged
+
+        ' Hiragana without dakuten
+        If KanaSelector.GetItemCheckState(HIRAGANA) = CheckState.Checked Then
+            hiraganaEnabled = True
+        Else
+            hiraganaEnabled = False
+        End If
+
+        ' Dakuten hiragana
+        If KanaSelector.GetItemCheckState(1) = CheckState.Checked Then
+            hiraganaDakutenEnabled = True
+        Else
+            hiraganaDakutenEnabled = False
+        End If
+
+        ' Combo hiragana
+        If KanaSelector.GetItemCheckState(HIRAGANA_COMBO) = CheckState.Checked Then
+            hiraganaComboEnabled = True
+        Else
+            hiraganaComboEnabled = False
+        End If
+
+        ' Katakana without dakuten
+        If KanaSelector.GetItemCheckState(KATAKANA) = CheckState.Checked Then
+            katakanaEnabled = True
+        Else
+            katakanaEnabled = False
+        End If
+
+        ' Dakuten katakana
+        If KanaSelector.GetItemCheckState(KATAKANA_DAKUTEN) = CheckState.Checked Then
+            katakanaDakutenEnabled = True
+        Else
+            katakanaDakutenEnabled = False
+        End If
+
+        ' Combo katakana
+        If KanaSelector.GetItemCheckState(KATAKANA_COMBO) = CheckState.Checked Then
+            katakanaComboEnabled = True
+        Else
+            katakanaComboEnabled = False
+        End If
+
+        ' Katakana extended table
+        If KanaSelector.GetItemCheckState(KATAKANA_EXTENDED) = CheckState.Checked Then
+            katakanaExtendedEnabled = True
+        Else
+            katakanaExtendedEnabled = False
+        End If
+
+        NextKana()
+        AnswerInput.Focus()
+    End Sub
+End Class
